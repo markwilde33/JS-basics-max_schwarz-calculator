d257285761bf63cd63473db49b0094a810b78d21
Mark Wilde <markwilde29@gmail.com>
2020-05-15 13:41:26 +0100

    Added assignment files and completed assignment.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,5 @@
-alert('this works');
+let currentResult = 0;
+
+currentResult = currentResult + 99 / 3;
+
+outputResult(currentResult, '');
---
+++ assignment/assignment.js
@@ -0,0 +1,12 @@
+// Assignment solution
+
+const userInput = 33;
+
+let result;
+
+result = ((userInput + 17) * 3) / 2 - 1;
+
+console.log(result);
+
+alert(userInput);
+alert(result);
