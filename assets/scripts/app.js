b9356c4ce91f5149df18183d4c6c5b6d62112ed3
Mark Wilde <markwilde29@gmail.com>
2020-05-15 14:44:18 +0100

    Added a function to add 2 numbers.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,16 @@
+const defaultResult = 0;
 let currentResult = 0;
 
-currentResult = currentResult + 99 / 3;
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert(`The result is ${result}`);
+}
 
-outputResult(currentResult, '');
+add(3, 3);
+add(6, 6);
+
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
