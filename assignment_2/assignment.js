804e6dd5aae65e5f57ab33ee502bbbf1c872572c
Mark Wilde <markwilde29@gmail.com>
2020-05-16 11:18:43 +0100

    Completed assignment.

--- assignment_2/assignment.js
+++ assignment_2/assignment.js
@@ -1,1 +1,22 @@
 const task3Element = document.getElementById('task-3');
+
+function noParam() {
+  alert('Hi there');
+}
+
+function userName(name) {
+  alert(`Hi there ${name}`);
+}
+
+// noParam();
+userName('Clarice');
+
+task3Element.addEventListener('click', noParam);
+
+function someStrings(str1, str2, str3) {
+  const addStrings = `${str1} ${str2} ${str3}`;
+  return addStrings;
+}
+
+const result = someStrings('I ', 'love ', 'animals');
+alert(result);
